cb99143312481026015c7a5cf78609d21cd3e596
Weston Platter <westonplatter@gmail.com>
2012-09-15 12:57:39 -0400

    navigates to domain table. captures table state

--- script.rb
+++ script.rb
@@ -16,22 +16,29 @@ require 'csv'
 godaddy = YAML::load( File.open( 'secure/godaddy_accounts.yml' ) )
 accounts = godaddy["accounts"]
 
-log_filename = Dir.glob("log.txt").empty? ? "log.txt" : Time.now.strftime("%Y%m%dT%H%M%S%z")
-# @logger = CSV.open(log_filename, "w")
-
-
+# log_filename = Dir.glob("log.txt").empty? ? "log.txt" : Time.now.strftime("%Y%m%dT%H%M%S%z")
+log_filename = "log.txt"
+@logger = CSV.open(log_filename, "w")
 
 @browser = Watir::Browser.new :firefox
+
 accounts.each do |account|
+	
+	#login to godaddy
 	@browser.goto "http://godaddy.com"
 	@browser.link(text: "Log In").click
-
 	@browser.text_field(title: "Enter Username").set(account["username"])
 	@browser.text_field(title: "Enter Password").set(account["password"])
-	@browser.link(id: "pc-loginSubmitBtn").click
-end
+	@browser.link(id: "pc-loginSubmitBtn").click	
+	
+	# go to domains center
+	@browser.goto("https://mya.godaddy.com/products/ControlPanelLaunch/ControlPanelLaunch.aspx?accordionId=1&generic=true")
 
-def login(account)
+	# record all domains
+	
+	# table
+	table = @browser.table(id: 'ctl00_cphMain_DomainList_gvDomains')
+	@logger << table.strings
 end
 
 def list_domains
