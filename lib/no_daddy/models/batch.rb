e55f22bc836594f1624c6824bb40e874b090e86e
Weston Platter <westonplatter@gmail.com>
2012-09-26 16:42:12 -0400

    Batch methods to query related children Domain objects

--- lib/no_daddy/models/batch.rb
+++ lib/no_daddy/models/batch.rb
@@ -12,5 +12,17 @@ module NoDaddy
     field :ready,             type: Boolean
 
     field :operations, type: Array
+
+    def domains_expired
+      domains.where(:expire_date.lt => Time.now)
+    end
+
+    def domains_expiring_between(time1, time2)
+      domains.between(:expire_date => time1..time2)
+    end
+
+    def domains_active
+      domains.where(:status => 'Active')
+    end
   end
 end
